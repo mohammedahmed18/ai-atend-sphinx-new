2a067ccb9afb2a945f945ae2b5d4d53e96acd27b
mohammed ahmed <mohammed18200118@gmail.com>
2022-07-12 16:04:23 +0200

    fix stuff

--- app/Http/Controllers/api/AlertsController.php
+++ app/Http/Controllers/api/AlertsController.php
@@ -2,27 +2,43 @@
 
 namespace App\Http\Controllers\api;
 
-use App\AlertToCompany;
 use App\company;
 use App\Http\Controllers\Controller;
 use App\payment_details;
 use Illuminate\Http\Request;
+use Jerry\JWT\JWT;
 
 class AlertsController extends Controller
 {
-    //
-    public function try()
-    {
-        return "hii";
-    }
-    // public function get_company_id()
-    // {
-    // }
+
     public function send_to_comapany(Request $request)
     {
-        $company_id = $request->company_id;
-
-        $alerts = company::find($company_id)->alerts()->where('is_activate', 1)
+        $token = $request->header('access_token');
+        if (!$token) {
+            return response()->json([
+                "msg" => "no token provided"
+            ], 403);
+        }
+        try {
+            $payload = JWT::basic_decode($token , env('JWT_SECRET'));
+        } catch (\Exception $e) {
+            return response()->json([
+                "msg" => "invalid access"
+            ], 403);
+        }
+        $company_id = $payload['company_id'];
+        if (!$company_id) {
+            return response()->json([
+                "msg" => "invalid access"
+            ], 403);
+        }
+        $company = company::find($company_id);
+        if (!$company) {
+            return response()->json([
+                "msg" => "invalid access"
+            ], 403);
+        }
+        $alerts = $company->alerts()->where('is_activate', 1)
             ->whereDate('start_date', '<=', date("Y-m-d"))
             ->whereDate('end_date', '>=', date("Y-m-d"))
             ->get();
